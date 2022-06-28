1df9bdfde69283d644ee589b64d040f8d4e39cc5
Ernst Widerberg <ernst@sunet.se>
2022-06-28 14:36:06 +0200

    Adjust for Django < 3.0
    User model: https://github.com/django/django/blob/e541f2d05b88e58c18b82b622aacc38d670eb5f6/django/contrib/auth/models.py
    RemoteUserBackend: https://github.com/django/django/blob/e541f2d05b88e58c18b82b622aacc38d670eb5f6/django/contrib/auth/backends.py

--- src/niweb/apps/saml_external/backend.py
+++ src/niweb/apps/saml_external/backend.py
@@ -3,7 +3,7 @@ from django.contrib.auth.backends import RemoteUserBackend
 class XRemoteUserBackend(RemoteUserBackend):
     create_unknown_user = True
 
-    def configure_user(self, request, user, created=True):
+    def configure_user(self, request, user):
         user.is_superuser = True
         user.is_staff = True
         return user
